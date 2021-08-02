ae75b127c5c93d7330f1a1886b75c300066fe891
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-08-01 19:44:02 -0600

    Update styling

--- react-app/src/components/TutorialsPage/index.js
+++ react-app/src/components/TutorialsPage/index.js
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { FiHeart } from 'react-icons/fi';
+import { FiHeart } from "react-icons/fi";
 import { getTutorials, unloadTutorials } from "../../store/tutorials";
-import CreateTutorial from '../CreateTutorial';
+import CreateTutorial from "../CreateTutorial";
 import styles from "./TutorialsPage.module.css";
 
 const TutorialsPage = () => {
   const dispatch = useDispatch();
   const [search, setSearch] = useState("");
 
-  let allTutorials = useSelector(state => Object.values(state.tutorials.all))
+  let allTutorials = useSelector((state) => Object.values(state.tutorials.all));
 
   useEffect(() => {
     dispatch(getTutorials());
@@ -17,10 +17,10 @@ const TutorialsPage = () => {
   }, [dispatch]);
 
   const searchFeature = () => {
-    return allTutorials.filter(tutorial => (
+    return allTutorials.filter((tutorial) =>
       tutorial.title.toLowerCase().includes(search.toLocaleLowerCase())
-    ))
-  }
+    );
+  };
 
   allTutorials = searchFeature();
 
@@ -28,35 +28,43 @@ const TutorialsPage = () => {
     <div className={styles.tutorialsPage}>
       <div className={styles.filterContainer}>Filter Div</div>
       <div className={styles.tutorialsDiv}>
-        <CreateTutorial />
-        <div className={styles.tutorialsSearch}>
-          <label htmlFor='searchVideos'>
-            <input
-              id='searchVideos'
-              className={styles.videoSearch}
-              placeholder="Search Videos"
-              type="search"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
+        <div className={styles.tutorialsTopDiv}>
+          <CreateTutorial />
+          <div className={styles.tutorialsSearch}>
+            <label htmlFor="searchVideos">
+              <input
+                id="searchVideos"
+                className={styles.videoSearch}
+                placeholder="Search Videos"
+                type="search"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
               />
-          </label>
+            </label>
+          </div>
         </div>
+
         <div className={styles.tutorialsContainer}>
-          {allTutorials && allTutorials.map((tutorial) => (
-            <a href={`/tutorials/${tutorial.id}`} key={tutorial.id}>
-              <div className={styles.videoCard}>
-                <div className={styles.cardTop}>
-                    <img className={styles.thumbnailImg} src={tutorial.thumbnailUrl} alt='video thumbnail' />
-                </div>
-                <div className={styles.cardBottom}>
-                  <div className={styles.videoTitle}>{tutorial.title}</div>
-                  <div className={styles.likeButton}>
-                    <FiHeart />
+          {allTutorials &&
+            allTutorials.map((tutorial) => (
+              <a href={`/tutorials/${tutorial.id}`} key={tutorial.id}>
+                <div className={styles.videoCard}>
+                  <div className={styles.cardTop}>
+                    <img
+                      className={styles.thumbnailImg}
+                      src={tutorial.thumbnailUrl}
+                      alt="video thumbnail"
+                    />
+                  </div>
+                  <div className={styles.cardBottom}>
+                    <div className={styles.videoTitle}>{tutorial.title}</div>
+                    <div className={styles.likeButton}>
+                      <FiHeart />
+                    </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          ))}
+              </a>
+            ))}
         </div>
       </div>
     </div>
