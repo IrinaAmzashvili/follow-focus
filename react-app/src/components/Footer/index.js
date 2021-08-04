30109afba9a32030a0aedf0fbf180681fa1c718c
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-08-03 19:31:33 -0600

    Add developer links to footer

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,10 +1,22 @@
+import { VscGithub } from 'react-icons/vsc';
+import { SiLinkedin } from 'react-icons/si';
 import styles from './Footer.module.css';
 
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>hello from footer</p>
+      <div className={styles.developerDiv}>
+        <p className={styles.devName}>Irina Amzashvili</p>
+        <div className={styles.developerLinks}>
+          <a href='https://github.com/IrinaAmzashvili' target='_blank'>
+            <VscGithub />
+          </a>
+          <a href='https://www.linkedin.com/in/irina-amzashvili-683136211/' target='_blank'>
+            <SiLinkedin />
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
