fede593f9987db58a765ca8f459b68f12dd66daf
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-08-06 01:20:21 -0600

    Refactor to put on single page and successfully debug - filter is working

--- react-app/src/components/TutorialsPage/index.js
+++ react-app/src/components/TutorialsPage/index.js
@@ -6,6 +6,8 @@ import loadingAnimation from "../../lotties/loading-dots-in-yellow.json";
 import { getTutorials, unloadTutorials } from "../../store/tutorials";
 import CreateTutorial from "../CreateTutorial";
 import FilterTutorials from "../FilterTutorials";
+import { getTutorialLevels } from "../../store/tutorialLevels";
+import { getDanceStyles } from "../../store/danceStyles";
 import styles from "./TutorialsPage.module.css";
 
 const TutorialsPage = () => {
@@ -59,12 +61,6 @@ const TutorialsPage = () => {
     setStart(0);
   }, [search]);
 
-  // display only 16 videos at a time
-  let tutorialsToDisplay;
-  if (isLoaded) {
-    tutorialsToDisplay = allTutorials.slice(start, start + 16);
-  }
-
   // show next 16 and previous 16 videos
   const handleNext = () => {
     setStart((prev) => prev + 16);
@@ -73,9 +69,158 @@ const TutorialsPage = () => {
     setStart((prev) => prev - 16);
   };
 
+  /***************************************************************************************/
+
+  /************************* Filter by dance style *************************/
+  const [allStylesChecked, setAllStylesChecked] = useState(true);
+  const [checkedStyles, setCheckedStyles] = useState([]);
+  const danceStyles = useSelector((state) => Object.values(state.danceStyles));
+
+  useEffect(() => {
+    dispatch(getDanceStyles());
+  }, [dispatch]);
+
+  const handleCheckedStyles = (e) => {
+    const arr = [...checkedStyles];
+    if (e.target.checked) {
+      arr.push(e.target.value);
+    } else {
+      const index = arr.indexOf(e.target.value);
+      arr.splice(index, 1);
+    }
+    setCheckedStyles(arr);
+    setAllStylesChecked(false);
+  };
+
+  if (isLoaded) {
+    if (checkedStyles.length) {
+      allTutorials = allTutorials.filter((tutorial) =>
+        checkedStyles.includes(tutorial.styleId.toString())
+      );
+    }
+  }
+
+  const handleAllStylesChecked = () => {
+    setCheckedStyles([]);
+    setAllStylesChecked(true);
+  };
+
+  useEffect(() => {
+    if (!checkedStyles.length) setAllStylesChecked(true);
+  }, [checkedStyles]);
+
+  /************************* Filter by tutorial levels *************************/
+  const [allLevelsChecked, setAllLevelsChecked] = useState(true);
+  const [checkedLevels, setCheckedLevels] = useState([]);
+  const tutorialLevels = useSelector((state) =>
+    Object.values(state.tutorialLevels)
+  );
+
+  useEffect(() => {
+    dispatch(getTutorialLevels());
+  }, [dispatch]);
+
+  const handleCheckedLevels = (e) => {
+    const arr = [...checkedLevels];
+    if (e.target.checked) {
+      arr.push(e.target.value);
+    } else {
+      const index = arr.indexOf(e.target.value);
+      arr.splice(index, 1);
+    }
+    setCheckedLevels(arr);
+    setAllLevelsChecked(false);
+  };
+
+  if (isLoaded) {
+    if (checkedLevels.length) {
+      allTutorials = allTutorials.filter((tutorial) =>
+        checkedLevels.includes(tutorial.levelId.toString())
+      );
+    }
+  }
+  console.log("allTutorials--->", allTutorials);
+
+  const handleAllLevelsChecked = () => {
+    setCheckedLevels([]);
+    setAllLevelsChecked(true);
+  };
+
+  useEffect(() => {
+    if (!checkedLevels.length) setAllLevelsChecked(true);
+  }, [checkedLevels]);
+
+
+
+    // display only 16 videos at a time
+  // const [tutorialsToDisplay, setTutorialsToDisplay] = useState();
+  let tutorialsToDisplay;
+  if (isLoaded) {
+    tutorialsToDisplay = allTutorials.slice(start, start + 16);
+    // setTutorialsToDisplay(allTutorials.slice(start, start + 16));
+  }
+
   return (
     <div className={styles.tutorialsPage}>
-      <FilterTutorials isLoaded={isLoaded} allTutorials={allTutorials} />
+      <div className={styles.filterContainer}>
+        <div className={styles.allFiltersDiv}>
+          <ul className={styles.filterDiv}>
+            <p className={styles.filterTitles}>Dance Styles:</p>
+            <li className={styles.checkboxAndLabel}>
+              <label htmlFor="all-styles">All Styles</label>
+              <input
+                id="all-styles"
+                type="checkbox"
+                checked={allStylesChecked}
+                onChange={handleAllStylesChecked}
+              />
+            </li>
+
+            {danceStyles &&
+              danceStyles.map((style, i) => (
+                <li className={styles.checkboxAndLabel} key={i}>
+                  <label htmlFor={style.danceStyle}>{style.danceStyle}</label>
+                  <input
+                    id={style.danceStyle}
+                    type="checkbox"
+                    checked={allStylesChecked ? false : null}
+                    value={style.id}
+                    onChange={handleCheckedStyles}
+                  />
+                </li>
+              ))}
+          </ul>
+
+          <ul className={styles.filterDiv}>
+            <p className={styles.filterTitles}>Levels:</p>
+            <li className={styles.checkboxAndLabel}>
+              <label htmlFor="all-levels">All Levels</label>
+              <input
+                id="all-levels"
+                type="checkbox"
+                checked={allLevelsChecked}
+                onChange={handleAllLevelsChecked}
+              />
+            </li>
+
+            {tutorialLevels &&
+              tutorialLevels.map((level, i) => (
+                <li className={styles.checkboxAndLabel} key={i}>
+                  <label htmlFor={level.levelType}>{level.levelType}</label>
+                  <input
+                    id={level.levelType}
+                    type="checkbox"
+                    checked={allLevelsChecked ? false : null}
+                    value={level.id}
+                    onChange={handleCheckedLevels}
+                  />
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
+
+
 
       <div className={styles.tutorialsDiv}>
         <div className={styles.tutorialsTopDiv}>
