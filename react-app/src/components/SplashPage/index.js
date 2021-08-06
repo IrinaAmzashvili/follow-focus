4b7a015694ccfe2a7a345929d7b6aa2d2b9299a0
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-08-06 10:45:15 -0600

    Update splash page scroll to top to only display when not at top of page

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,25 +1,51 @@
+import { useState } from "react";
 import { Redirect } from "react-router-dom";
-import ReactPlayer from 'react-player';
-import SplashVideo from '../../videos/countsIdea.mp4';
-import styles from './SplashPage.module.css';
+import ReactPlayer from "react-player";
+import { Waypoint } from "react-waypoint";
+import { HashLink as Link } from "react-router-hash-link";
+import { AiOutlineArrowUp } from "react-icons/ai";
+import SplashVideo from "../../videos/countsIdea.mp4";
+import styles from "./SplashPage.module.css";
 
 const SplashPage = ({ sessionUser }) => {
+  const [topOfPage, setTopOfPage] = useState(true);
+
+  const backToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   if (sessionUser) {
     return <Redirect to="/tutorials" />;
   }
+
   return (
-    <div className={styles.videoDiv}>
-      <ReactPlayer
-        className={styles.video}
-        url={SplashVideo}
-        playing={true}
-        muted={true}
-        loop={true}
-        width='100%'
-        height='93vh'
-        controls={false}
-      />
-    </div>
+    <>
+      <Waypoint bottomOffset='20%' onEnter={() => setTopOfPage(true)} onLeave={() => setTopOfPage(false)}>
+
+      <div className={styles.videoDiv}>
+        <ReactPlayer
+          className={styles.video}
+          url={SplashVideo}
+          playing={true}
+          muted={true}
+          loop={true}
+          width="100%"
+          height="93vh"
+          controls={false}
+          />
+      </div>
+      </Waypoint>
+      {!topOfPage && (
+        <button
+          onClick={backToTop}
+          className={`link-button icon-button ${styles.backToTopButton}`}
+        >
+          <Link smooth to="/" exact="true">
+            <AiOutlineArrowUp />
+          </Link>
+        </button>
+      )}
+    </>
   );
 };
 
