b97c586e19279e2208ca70a49ba8f25264ae997d
IrinaAmzashvili <irina.amzashvili@gmail.com>
2021-08-11 00:00:02 -0600

    Add length validator on comments

--- app/forms/comment_form.py
+++ app/forms/comment_form.py
@@ -5,7 +5,7 @@ from app.models import Comment
 
 
 class CommentForm(FlaskForm):
-    body = StringField('body', validators=[DataRequired()])
+    body = StringField('body', validators=[DataRequired(), Length(min=1, max=4000, message='Comment must bebetween 1 and 4000 characters long.')])
     user_id = StringField('user_id', validators=[DataRequired()])
     tutorial_id = StringField('tutorial_id', validators=[DataRequired()])
     created_at = StringField('created_at', validators=[DataRequired()])
